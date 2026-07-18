2dc4f3ac0e18214a4ffa28307f396aee21388fcf
gangasanihemalatha1-del <gangasanihemalatha1@gmail.com>
2026-07-18 22:04:13 +0530

    Update spotify_analysis.py

--- spotify_analysis.py
+++ spotify_analysis.py
@@ -1,2 +1,5 @@
 import pandas as pd
-print("Spotify Data project Initialized!")
+ur 1 ="https://githubusercontent.com"
+df=pd.read_csv(ur1)
+print("spotify Data successfully loaded!")
+print(f"The dataset contains {df.shape[0]}songs and {df.shape[1]}columns.")
