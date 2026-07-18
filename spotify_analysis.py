a83cfa0a3182455bf31bfa9b332c578b18af637d
gangasanihemalatha1-del <gangasanihemalatha1@gmail.com>
2026-07-18 22:07:51 +0530

    Update spotify_analysis.py

--- spotify_analysis.py
+++ spotify_analysis.py
@@ -1,5 +1,9 @@
 import pandas as pd
+print("spotify Data project Initialized!")
 ur 1 ="https://githubusercontent.com"
 df=pd.read_csv(ur1)
 print("spotify Data successfully loaded!")
 print(f"The dataset contains {df.shape[0]}songs and {df.shape[1]}columns.")
+#show the names of all variables we can analyze
+print("available attributes to  look at:")
+print(list(df.columns))
